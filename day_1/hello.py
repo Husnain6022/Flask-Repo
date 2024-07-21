435483af37b30fac935cd486c032ae08e3d7e27e
Husnain022 <ihusnain844@gmail.com>
2024-07-21 22:42:40 +0500

    My Name Card Static Site

--- day_1/hello.py
+++ day_1/hello.py
@@ -1,28 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template("index.html")
 
-@app.route("/bye/<name>")       #(default) accepts any text without a slash  (String)
-def string_world(name):
-    return f"<p>Bye, {name}</p>"
-
-@app.route("/number/<int:number>")  #accepts positive integers
-def int_world(number):
-    return f"<p>Here is the number {number}</p>"
-
-@app.route("/float/<float:float_number>")  #accepts positive floating point values
-def float_world(float_number):
-    return f"<p>Here is the float number {float_number}</p>"
-
-
-
-@app.route("/path/<path:subpath>")  #like string but also accepts slashes
-def path_world(subpath):
-    return f"<p>Here is the path {subpath}</p>"
 
 
 if __name__ == "__main__":
